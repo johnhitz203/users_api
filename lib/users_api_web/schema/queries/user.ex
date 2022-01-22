1a16e547300037803f09078dfcc49fe2a09bd529
johnhitz203 <johnhitz203@gmail.com>
2022-01-22 10:37:31 -0600

    Renamed /schema and /queries directories to use lower case names.

--- lib/users_api_web/schema/queries/user.ex
+++ lib/users_api_web/schema/queries/user.ex
@@ -7,6 +7,13 @@ defmodule UsersApiWeb.Schema.Queries.User do
     @desc """
     Returns a user or an error with an id: in the datails field and 'not found'
     in the message field if no user is found.
+
+    Query with: /n
+    query {
+      user(id: id) {
+        fields...
+      }
+    }
     """
     field :user, :user do
       arg :id, non_null(:id)
@@ -14,7 +21,16 @@ defmodule UsersApiWeb.Schema.Queries.User do
     resolve &Resolvers.User.find/2
     end
 
-    @desc "Users returns a list of users or an empty list."
+    @desc """
+    Users returns a list of users or an empty list.
+    Query with:
+
+    query {
+      users(likesEmails: boolean, likesPhoneCalls: boolean){
+        fields
+      }
+    }
+    """
     field :users, list_of(:user) do
       arg :likes_emails, :boolean
       arg :likes_phone_calls, :boolean
