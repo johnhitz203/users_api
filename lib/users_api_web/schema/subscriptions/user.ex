790383a6c9cc7ca44e2ab733caa43a47d62f6366
johnhitz203 <johnhitz203@gmail.com>
2022-01-25 10:19:39 -0600

    Subscription for updatedUserPreferences(userId: 1) working

--- lib/users_api_web/schema/subscriptions/user.ex
+++ lib/users_api_web/schema/subscriptions/user.ex
@@ -2,7 +2,7 @@ defmodule UsersApiWeb.Schema.Subscriptions.User do
   use Absinthe.Schema.Notation
 
   object :user_subscriptions do
-    field :user_created, :user do
+    field :created_user, :user do
       trigger :create_user, topic: fn _ ->
         "new_user"
       end
@@ -11,5 +11,18 @@ defmodule UsersApiWeb.Schema.Subscriptions.User do
         {:ok, topic: "new_user"}
       end
     end
+
+    field :updated_user_preferences, :preferences do
+      arg :user_id, :id
+      trigger :update_user_preferences, topic: fn _ ->
+        "updated preferences"
+      end
+
+      config fn _, _ ->
+        {:ok, topic: "updated preferences"}
+      end
+    end
+
+
   end
 end
